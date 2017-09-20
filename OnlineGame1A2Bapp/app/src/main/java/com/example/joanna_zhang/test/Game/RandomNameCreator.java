6c0ff6e16568d217ae3beb960461b42de4403783
林宗億 <tonylin601@gmail.com>
2017-09-20 19:39:46 +0800

    create random name

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/Game/RandomNameCreator.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/Game/RandomNameCreator.java
@@ -1,11 +1,19 @@
 package com.example.joanna_zhang.test.Game;
 
+import com.example.joanna_zhang.test.NameCreator.NameCreator;
+
 /**
  * Created by Lin on 2017/9/17.
  */
 
-public class RandomNameCreator {
+public class RandomNameCreator implements NameCreator {
 
-    private String[] names = {"莫載啼", "莫載妏", "遊戲王", ""};
+    private String[] names = {"莫載啼", "莫載妏", "遊戲王", "林筱珍", "包龍興", "橙汁內", "墨 海馬"};
 
+    @Override
+    public String createRandomName() {
+        String name = names[(int) (Math.random() * names.length)];
+
+        return name;
+    }
 }
