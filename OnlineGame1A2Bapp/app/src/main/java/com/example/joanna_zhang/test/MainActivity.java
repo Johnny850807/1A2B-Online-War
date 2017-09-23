17a684e038f9e2aa2424b3f9b26733502b06ddd6
xuan1280 <xuan1280@gmail.com>
2017-09-23 22:02:47 +0800

    登入模組變好看 讚讚!!!

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/MainActivity.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.joanna_zhang.test.Game.RandomNameCreator;
 import com.ood.clean.waterball.a1a2bsdk.core.CoreGameServer;
@@ -19,7 +18,7 @@ import com.ood.clean.waterball.a1a2bsdk.core.model.User;
 import com.ood.clean.waterball.a1a2bsdk.core.modules.signIn.UserSigningModule;
 import com.ood.clean.waterball.a1a2bsdk.core.modules.signIn.exceptions.UserNameFormatException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements UserSigningModule.Callback {
 
     private EditText editText;
     private CheckBox checkBox;
@@ -46,21 +45,7 @@ public class MainActivity extends AppCompatActivity {
         CoreGameServer server = CoreGameServer.getInstance();
         server.startEngine(MainActivity.this);
         UserSigningModule signingModule = (UserSigningModule) server.getModule(ModuleName.SIGNING);
-        signingModule.signIn(name , new UserSigningModule.Callback() {
-            @Override
-            public void onSignInSuccessfully(@NonNull User user) {
-                Intent intent = new Intent(MainActivity.this, RoomListActivity.class);
-                intent.putExtra("user", user); // send the user data to the next activity
-                startActivity(intent);
-            }
-
-            @Override
-            public void onSignInFailed(@NonNull Exception err) throws UserNameFormatException {
-                if (err instanceof UserNameFormatException)
-                    errorMessage(String.valueOf(R.string.signInFailedMessage));
-            }
-        });
-
+        signingModule.signIn(name, this);
         //todo if (checkBox.isChecked());
     }
 
@@ -79,4 +64,18 @@ public class MainActivity extends AppCompatActivity {
     public void randomNameButtonOnClick(View view) {
         editText.setText(randomNameCreator.createRandomName());
     }
+
+    @Override
+    public void onSignInSuccessfully(@NonNull User user) {
+        Intent intent = new Intent(this, RoomListActivity.class);
+        intent.putExtra("user", user); // send the user data to the next activity
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSignInFailed(@NonNull Exception err) {
+        if (err instanceof UserNameFormatException)
+            errorMessage(String.valueOf(R.string.signInFailedMessage));
+    }
+
 }
