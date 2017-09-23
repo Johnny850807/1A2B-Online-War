0a94b868daa8910f1eeeddc35cf5e9782517cd21
林宗億 <tonylin601@gmail.com>
2017-09-23 15:04:14 +0800

    change activity

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/MainActivity.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.joanna_zhang.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.ood.clean.waterball.a1a2bsdk.core.model.Room;
+import com.ood.clean.waterball.a1a2bsdk.core.model.User;
 
 import java.util.Random;
 
@@ -15,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkBox;
     private TextView serverStatus;
     private String name;
-    public static String[] randomNames = {"嗨你好", "丁丁", "拉拉", "小波"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+
     }
 
     private void findViews() {
@@ -30,16 +34,12 @@ public class MainActivity extends AppCompatActivity {
         serverStatus = (TextView) findViewById(R.id.serverStatus);
     }
 
-    public void randomNameButtonOnClick(View view) {
-        Random random = new Random();
-        String name = randomNames[random.nextInt(randomNames.length)];
-        editText.setText(name);
-    }
-
     public void loginButtonOnClick(View view) {
         name = editText.getText().toString();
+        Intent activity = new Intent(MainActivity.this, RoomList.class);
+        activity.putExtra("user", new User(name));
+        startActivity(activity);
         //if (checkBox.isChecked());
     }
-
 
 }
