65a15ad8a2e104f3e899cef8c09503959b8b067f
WaterBall <johnny850807@gmail.com>
2017-10-14 17:52:00 +0800

    remove all duplicated entity class and use the class providede be module

--- OnlineGame1A2Bapp/1a2bsdk/src/main/java/com/ood/clean/waterball/a1a2bsdk/core/model/ChatMessage.java
+++ OnlineGame1A2Bapp/1a2bsdk/src/main/java/com/ood/clean/waterball/a1a2bsdk/core/model/ChatMessage.java
@@ -6,11 +6,7 @@ import java.util.Date;
 public class ChatMessage extends SerializableEntity{
     private Player poster;
     private String content;
-    private Date postDate;
-
-    public ChatMessage(){
-        postDate = new Date();
-    }
+    private Date postDate  = new Date();
 
     public ChatMessage(Player poster, String content) {
         this.poster = poster;
