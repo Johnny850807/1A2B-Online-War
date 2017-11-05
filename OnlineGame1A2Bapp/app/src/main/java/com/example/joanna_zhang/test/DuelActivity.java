11b3bfac03e4bbea15513fa79f4687ac38f3b461
xuan1280 <xuan1280@gmail.com>
2017-11-05 22:34:12 +0800

    pull

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/DuelActivity.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/DuelActivity.java
@@ -2,6 +2,7 @@ package com.example.joanna_zhang.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
