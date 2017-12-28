ebda1532293078ace66ab1315a91060bb614a10a
xuan1280 <xuan1280@gmail.com>
2017-12-29 01:17:37 +0800

    duelgame connect moudle test

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/DuelActivity.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/DuelActivity.java
@@ -1,7 +1,10 @@
 package com.example.joanna_zhang.test;
 
-import android.content.Intent;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +13,11 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ood.clean.waterball.a1a2bsdk.core.CoreGameServer;
 import com.ood.clean.waterball.a1a2bsdk.core.ModuleName;
+import com.ood.clean.waterball.a1a2bsdk.core.modules.games.Duel1A2BModule;
 import com.ood.clean.waterball.a1a2bsdk.core.modules.roomlist.RoomListModule;
 import com.ood.clean.waterball.a1a2bsdk.core.modules.signIn.UserSigningModule;
 
@@ -21,12 +26,16 @@ import java.util.List;
 import gamecore.entity.ChatMessage;
 import gamecore.entity.GameRoom;
 import gamecore.entity.Player;
+import gamecore.model.ContentModel;
+import gamecore.model.games.a1b2.Duel1A2BPlayerBarModel;
+import gamecore.model.games.a1b2.GameOverModel;
+import gamecore.model.games.a1b2.GuessRecord;
 
-public class DuelActivity extends AppCompatActivity implements ChatWindowView.ChatMessageListener, InputNumberWindowView.OnClickListener {
+public class DuelActivity extends AppCompatActivity implements ChatWindowView.ChatMessageListener, InputNumberWindowView.OnClickListener, Duel1A2BModule.Callback {
 
 
-    //private Duel1A2BGameModule duel1A2BGameModule;
-    //private List<GuessRecord> p1ResultList, p2ResultList;
+    private Duel1A2BModule duel1A2BModule;
+    private List<GuessRecord> p1ResultList, p2ResultList;
     private GameRoom currentGameRoom;
     private Player currentPlayer;
     private ChatWindowView chatWindowView;
@@ -34,22 +43,45 @@ public class DuelActivity extends AppCompatActivity implements ChatWindowView.Ch
     private Button inputNumberBtn;
     private TextView p1NameTxt, p2NameTxt, p1AnswerTxt, p2AnswerTxt;
     private ListView p1ResultListView, p2ResultListView;
-    private List<String> p1Results, p2Results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_duel);
+        init();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        duel1A2BModule.registerCallback(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        duel1A2BModule.unregisterCallBack(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    private void init() {
+        setUpRoomInformation();
+        setupAnswer();
+        findViews();
+        setUpChatWindow();
+        setUpInputNumberWindowView();
+    }
+
+    private void setUpRoomInformation() {
         CoreGameServer server = CoreGameServer.getInstance();
-        //duel1A2BGameModule = (Duel1A2BGameModule) server.getModule(ModuleName.SIGNING);
+        duel1A2BModule = (Duel1A2BModule) server.getModule(ModuleName.GAME1A2BDUEL);
         currentPlayer = ((UserSigningModule) CoreGameServer.getInstance().getModule(ModuleName.SIGNING)).getCurrentPlayer();
         currentGameRoom = ((RoomListModule) CoreGameServer.getInstance().getModule(ModuleName.ROOMLIST)).getCurrentGameRoom();
-        Intent intent = getIntent();
-
-        setupChatWindow();
-        findViews();
-        setupAnswer();
     }
 
     private void findViews() {
@@ -64,67 +96,111 @@ public class DuelActivity extends AppCompatActivity implements ChatWindowView.Ch
 
     private void setupAnswer() {
         new InputNumberWindowView.Builder(this)
-                .setOnEnterClickListener(guessNumber -> p1AnswerTxt.setText(guessNumber))
+                .setOnEnterClickListener(answer -> duel1A2BModule.setAnswer(answer))
                 .setCanceledOnTouchOutside(false)
                 .setCancelable(false)
                 .setTitle(getString(R.string.setAnswerFrist))
                 .show();
     }
 
-    public void setupChatWindow() {
+    public void setUpChatWindow() {
         chatWindowView = new ChatWindowView.Builder(this, currentGameRoom, currentPlayer)
                 .addOnSendMessageOnClickListener(this)
                 .build();
     }
 
-    private void setupInputNumberWindowView() {
+    private void setUpInputNumberWindowView() {
         inputNumberWindowView = new InputNumberWindowView.Builder(this)
                 .setOnEnterClickListener(this)
                 .setTitle("請輸入答案")
-                .show();
+                .build();
     }
-/*
+
     public void updateResultList(List<GuessRecord> resultList, ListView resultListView) {
-        ResultListAdapter adapter = new ResultListAdapter(resultList);
+        GuessResultAdapter adapter = new GuessResultAdapter(resultList);
         resultListView.setAdapter(adapter);
         resultListView.setSelection(resultListView.getCount() - 1);
-    }*/
-/*
-    private Player getCurrentPlayer() {
-        CoreGameServer server = CoreGameServer.getInstance();
-        UserSigningModule signingModule = (MockUserSigningModule) server.getModule(ModuleName.SIGNING);
-        return signingModule.getCurrentPlayer();
-    }*/
-
-    @Override
-    public void onChatMessageUpdate(ChatMessage chatMessage) {
-
     }
 
     @Override
-    public void onMessageSendingFailed(ChatMessage chatMessage) {
-
-    }
+    public void onChatMessageUpdate(ChatMessage chatMessage) {}
 
     @Override
-    public void onChatMessageError(Throwable err) {
+    public void onMessageSendingFailed(ChatMessage chatMessage) {}
 
-    }
+    @Override
+    public void onChatMessageError(Throwable err) {}
 
     @Override
     public void onEnterClick(String guessNumber) {
         inputNumberBtn.setText(guessNumber);
+        duel1A2BModule.guess(guessNumber);
     }
 
     public void inputNumberOnClick(View view) {
-        setupInputNumberWindowView();
+        inputNumberWindowView.show();
+    }
+
+    @Override
+    public void onSetAnswerSuccessfully(ContentModel setAnswerModel) {
+        Toast.makeText(this, "You set the number is" + setAnswerModel.getContent(), Toast.LENGTH_SHORT).show();
+        p1AnswerTxt.setText(setAnswerModel.getContent());
+    }
+
+    @Override
+    public void onSetAnswerUnsuccessfully(ContentModel setAnswerModel) {
+
+    }
+
+    @Override
+    public void onGuessSuccessfully(ContentModel guessModel) {
+        Toast.makeText(this, "Your guess number is" + guessModel.getContent(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onGuessUnsuccessfully(ContentModel guessModel) {
+
+    }
+
+    @Override
+    public void onGuessingStarted() {
+        inputNumberBtn.setEnabled(true);
+        Toast.makeText(this, "開始猜", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onOneRoundOver(List<Duel1A2BPlayerBarModel> models) {
+        p1ResultList = models.get(0).getGuessRecords();
+        p2ResultList = models.get(1).getGuessRecords();
+        updateResultList(p1ResultList, p1ResultListView);
+        updateResultList(p2ResultList, p2ResultListView);
+    }
+
+    @Override
+    public void onGameOver(GameOverModel gameOverModel) {
+        new AlertDialog.Builder(DuelActivity.this)
+                .setTitle(R.string.gameOver)
+                .setMessage(gameOverModel.getWinnerId())
+                .setIcon(R.drawable.logo)
+                .setPositiveButton(R.string.confirm,  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onError(@NonNull Throwable err) {
+        Toast.makeText(this, err.getMessage(), Toast.LENGTH_LONG).show();
     }
 
     private class GuessResultAdapter extends BaseAdapter {
 
-        private List<String> resultList;
+        private List<GuessRecord> resultList;
 
-        GuessResultAdapter(List<String> resultList) {
+        GuessResultAdapter(List<GuessRecord> resultList) {
             this.resultList = resultList;
         }
 
@@ -147,8 +223,14 @@ public class DuelActivity extends AppCompatActivity implements ChatWindowView.Ch
         public View getView(int i, View view, ViewGroup viewGroup) {
             view = LayoutInflater.from(DuelActivity.this).inflate(android.R.layout.simple_list_item_1, viewGroup, false);
 
-            // TODO
-            return null;
+            TextView resultTxt = findViewById(android.R.id.text1);
+            StringBuilder result = new StringBuilder();
+            result.append(resultList.get(i).getGuess())
+                    .append('-')
+                    .append(resultList.get(i).getResult().toString());
+            resultTxt.setText(result);
+
+            return view;
         }
     }
 
