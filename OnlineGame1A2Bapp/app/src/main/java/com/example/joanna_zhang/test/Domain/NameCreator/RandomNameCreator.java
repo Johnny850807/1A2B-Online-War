dceb9cc2e1dde4ff811b3a44c3042a2722c9f1ff
johnny850807 <johnny850807@gmail.com>
2017-12-30 19:32:33 +0800

    finish

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/Domain/NameCreator/RandomNameCreator.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/Domain/NameCreator/RandomNameCreator.java
@@ -4,7 +4,7 @@ public class RandomNameCreator implements NameCreator {
     String[] names = {"Sponge", "Jackson", "Tony", "Sunny", "Java", "Bob", "John", "Emma", "Sans",
     "Chara", "Cindy", "Steve", "Lin", "Tina", "Pica", "Sheng", "ArPing", "Joanna", "Ben", "Nick", "Grace",
     "Ray", "Kate", "Linda", "Vivi", "Rosa", "Yen", "Yin", "Ian", "Jack", "Davis", "Dennis", "Woody", "Julian", "Justin",
-    "Deep", "Freeze", "Firen", "Louis", "Coco", "June"};
+    "Deep", "Freeze", "Firen", "Louis", "Coco", "June", "YoJin", "ZongYe", "Water", "Jeff", "Yane"};
 
     @Override
     public String createRandomName() {
