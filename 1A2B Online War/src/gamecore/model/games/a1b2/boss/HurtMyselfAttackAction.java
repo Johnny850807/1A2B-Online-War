9eb358773f9755bd9ad7ad403d543d5c4f0b9157
Shawn Wu <swshawnwu@gmail.com>
2018-01-02 20:25:34 +0800

    MonsterAction From Shawn

---
+++ 1A2B Online War/src/gamecore/model/games/a1b2/boss/HurtMyselfAttackAction.java
@@ -0,0 +1,40 @@
+package gamecore.model.games.a1b2.boss;
+import gamecore.model.games.a1b2.GuessResult;
+import gamecore.model.games.a1b2.boss.AbstractSpirit.DamageParser;
+
+import gamecore.model.games.a1b2.boss.AttackResult.AttackType;
+
+public class HurtMyselfAttackAction extends AbstractMonsterAction{
+
+	@Override
+	public void execute(Monster monster, Boss1A2BGame game) {
+		PlayerSpirit targetPlayer = getTargetPlayer(game);
+		String guess = getGuess();
+		AttackResult attackResult = targetPlayer.getAttacked(monster, guess, AttackType.MAGIC);
+		AttackActionModel model = new AttackActionModel(getCostMp(), monster);
+		model.addAttackResult(attackResult);
+		game.broadcastAttackActionModel(model);
+		
+	}
+	
+	
+	@Override
+	public int getCostMp() {
+		return 200;
+	}
+	
+	public DamageParser getHurtMyselfDamageParser(Monster monster){
+		return new DamageParser() {
+			
+			@Override
+			public int parsingDamage(GuessResult guessResult) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		};
+			
+		
+	}
+
+	
+}
