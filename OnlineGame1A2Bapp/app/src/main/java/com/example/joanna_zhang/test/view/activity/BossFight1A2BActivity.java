e4d1dd0a1515a323c3df5668fb3a847b8e42c4b9
xuan1280 <xuan1280@gmail.com>
2018-01-06 23:29:13 +0800

    player background dark

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/view/activity/BossFight1A2BActivity.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/view/activity/BossFight1A2BActivity.java
@@ -236,10 +236,12 @@ public class BossFight1A2BActivity extends OnlineGameActivity implements Boss1A2
         for (AbstractSpiritItemViewFactory.ViewHolder viewHolder : playerSpiritViewHoldersMap.values())
             viewHolder.view.setBackgroundColor(Color.GRAY);
 
+        playerSpiritViewHoldersMap.get(whosTurn.getId()).view.setBackgroundResource(R.drawable.boss1a2b_player_background);
+
         if (whosTurn.getId().equals(currentPlayer.getId()))
         {
-            playerSpiritViewHoldersMap.get(whosTurn.getId()).view.setBackgroundResource(R.drawable.boss1a2b_player_background);
             setInputNumberViewsEnabled(true);
+            inputNumberBtn.setText(null);
             soundManager.playSound(R.raw.dong);
         } else {
             setInputNumberViewsEnabled(false);
