0cc1ccf15de59b6a3f96a473ea09e7d2902424ed
johnny850807 <johnny850807@gmail.com>
2018-01-06 22:05:10 +0800

    pull

--- OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/view/activity/BossFight1A2BActivity.java
+++ OnlineGame1A2Bapp/app/src/main/java/com/example/joanna_zhang/test/view/activity/BossFight1A2BActivity.java
@@ -6,7 +6,6 @@ import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
